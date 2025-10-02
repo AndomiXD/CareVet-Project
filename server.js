e41cb5c129eb0566657db2741dbf0eb4fe832bcc
AndomiXD <vintin4some@gmail.com>
2025-10-02 09:05:30 +0300

    connected to CareVet DB

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require("express")
 require("dotenv").config()
 const app = express()
 const logger = require("morgan")
+const methodOverride = require("method-override")
+const session = require("express-session")
+const db = require("./db")
+require("dotenv").config()
+
 const PORT = process.env.PORT ? process.env.PORT : 3000
-// const session = require("express-session")
 
 app.use(logger("dev"))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
 
 app.use("/", (request, respond) => {
   respond.send("Root Route Working")

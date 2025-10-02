0109c7261d8fc59ae48149c3cef950a9e31ccfa9
mohamed-janahi <Mohamed.janahi98@gmail.com>
2025-10-02 16:05:54 +0300

    homepage implemented

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -8,9 +8,7 @@ router.get("/sign-up", (request, respond) => {
 })
 router.post("/sign-up", authCtrl.registerUser)
 
-
-router.get("/sign-out", authController.auth_signout_get)
-
+router.get("/sign-out", authCtrl.auth_signout_get)
 
 router.get("/sign-in", authCtrl.auth_signin_get)
 router.post("/sign-in", authCtrl.auth_signin_post)

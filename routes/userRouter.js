fc4d2b28f5b613b4d42750414a04d958f1b03233
emannn077 <emanrauf779@gmail.com>
2025-10-05 21:06:19 +0300

    fixed the userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,19 +5,13 @@ const userCtrl = require("../controllers/userController")
 const isSignedIn = require("../middleware/is-sign-in")
 
 // Routes
-router.get("/:id", userCtrl.getProfile)
-router.get("/:id/update-profile", userCtrl.update_profile_get)
-router.put("/:id", userCtrl.update_profile_put)
 
 router.get("/bookAppointment", isSignedIn, userCtrl.get_book_appointment)
-router.post(
-  "/bookAppointment",
-  isSignedIn,
-  userCtrl.post_book_appointment
-)
+router.post("/bookAppointment", isSignedIn, userCtrl.post_book_appointment)
+router.get("/viewAppointment", userCtrl.get_view_appointment)
 
-router.get("/viewAppointment", userController.get_view_appointment)
-
-router.get("/:id", userController.getProfile)
+router.get("/:id/update-profile", userCtrl.update_profile_get)
+router.put("/:id", userCtrl.update_profile_put)
+router.get("/:id", userCtrl.getProfile)
 
 module.exports = router

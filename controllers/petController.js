f203c150021728e10542d1da70a6dc23ab0e7043
mohamed-janahi <Mohamed.janahi98@gmail.com>
2025-10-05 16:37:33 +0300

    finished the show all pets

--- controllers/petController.js
+++ controllers/petController.js
@@ -2,8 +2,8 @@ const Pet = require("../models/Pet")
 
 exports.post_add_pet = async (req, res) => {
   try {
-    const petInDatabase = await Pet.findById(req.body._id)
-    // const ownerId = req.session.user._id
+    const petInDatabase = await Pet.findById(req.bodyid)
+    // const ownerId = req.session.userid
 
     if (petInDatabase) {
       return res.send("Pet already exists")
@@ -23,6 +23,25 @@ exports.post_add_pet = async (req, res) => {
   }
 }
 
+exports.getAllPets = async (req, res) => {
+  try {
+    const pets = await Pet.find({})
+
+    res.render("pets/all.ejs", { pets })
+  } catch (error) {
+    console.error("An error has occurred getting all pets!", error.message)
+  }
+}
+
+exports.getPetById = async (req, res) => {
+  try {
+    const pet = await Pet.findById(req.params.id)
+
+    res.render("./pets/show.ejs", { user: req.session.user, pet })
+  } catch (error) {
+    console.error("An error has occurred getting a pet!", error.message)
+  }
+}
 exports.get_add_pet = (req, res) => {
   res.render("pets/addPet.ejs")
 }

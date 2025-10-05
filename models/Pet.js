1894bc64bc3efb326b2c384e60a63523ca9f7420
AndomiXD <vintin4some@gmail.com>
2025-10-05 11:50:23 +0300

    profile,ejs update

--- models/Pet.js
+++ models/Pet.js
@@ -6,11 +6,11 @@ const petSchema = new mongoose.Schema(
     species: { type: String, required: true },
     breed: { type: String, required: true },
     petPhoto: String,
-    // owner: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "User",
-    //   required: true,
-    // },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   { timestamps: true }
 )

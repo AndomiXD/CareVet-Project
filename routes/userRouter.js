9a50dd97c039378a1a08235065547c1783eea22e
AndomiXD <vintin4some@gmail.com>
2025-10-07 07:16:12 +0300

    Added Edit and Delete Appointments

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,10 +4,6 @@ const router = express.Router()
 const userCtrl = require("../controllers/userController")
 const isSignedIn = require("../middleware/is-sign-in")
 
-router.get("/bookAppointment", isSignedIn, userCtrl.get_book_appointment)
-router.post("/bookAppointment", isSignedIn, userCtrl.post_book_appointment)
-router.get("/viewAppointment", userCtrl.get_view_appointment)
-
 // Routes
 router.get("/bookAppointment", isSignedIn, userCtrl.get_book_appointment)
 router.post("/bookAppointment", isSignedIn, userCtrl.post_book_appointment)
@@ -16,5 +12,10 @@ router.get("/viewAppointment", userCtrl.get_view_appointment)
 router.get("/:id/update-profile", userCtrl.update_profile_get)
 router.put("/:id", userCtrl.update_profile_put)
 router.get("/:id", userCtrl.getProfile)
+
+router.get("/appointments/:id/edit", userCtrl.get_edit_appointment)
+router.put("/appointments/:id", userCtrl.put_edit_appointment)
+
+router.delete("/appointments/:id", userCtrl.delete_appointment)
 
 module.exports = router
